8ed25c85a9ef0934370de38af1efba566efe764c
Mario Leyva <41600642+MarioTheEmblem@users.noreply.github.com>
2024-08-08 22:25:48 +0000

    Notebook Scraping Tables Terminado

---
+++ notebooks/20_Scraping Tables/21_Scrap/ModuloOmie.py
@@ -0,0 +1,14 @@
+
+import os
+import pandas as pd
+
+def descargar_omie_ruta(fecha, path=None):
+    URL = F'https://www.omip.pt/es/market-data/spot?date={fecha}&commodity=EL&zone=ES'
+    dfs = pd.read_html(URL)
+    df = dfs[1]
+    
+    if path:
+        filename = f'{fecha}.csv'
+        pathfile = os.path.join(path,filename)
+        df.to_csv(pathfile, index=False)
+    return df
