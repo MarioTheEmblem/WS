b09286aa8d12c41d6db7a3b560aef31e5f44c963
Mario Leyva <41600642+MarioTheEmblem@users.noreply.github.com>
2024-08-08 02:42:11 +0000

    Standby errores y nulo avance

--- alphavantage.py
+++ alphavantage.py
@@ -1,8 +1,6 @@
 import requests
 import pandas as pd
-
-def get_stock_data(symbol='IBM'):
-
+def preprocess(symbol):
     base = 'https://www.alphavantage.co'
     endpoint = '/query'
     URL=  base + endpoint
@@ -13,12 +11,10 @@ def get_stock_data(symbol='IBM'):
         "outputsize": "full",
         "apikey": 'G6J7BB76C6AR6WKQ'
     }
-
-    res = requests.get(URL,params)
+    res = requests.get(URL, params)
     data = res.json()
-
-    df =pd.DataFrame(data['Time Series (Daily)']).T.astype(float)
-    df.columns = ['open', 'high', 'low', 'close', 'volumen']
+    df = pd.DataFrame(data['Time Series (Daily)']).T.astype(float)
+    df.columns = ['open', 'high', 'low', 'close', 'volume']
     df.index = pd.to_datetime(df.index)
     df = df.sort_index()
     return df
